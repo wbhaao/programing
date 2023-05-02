a329ef8b1ce49d80c4bcdd3c32156abfa8313b24
wbhaao <a316204504@gmail.com>
2023-05-02 12:07:14 +0900

    그냥 동아리 회장인데 재귀 X

--- codeup/2608.c
+++ codeup/2608.c
@@ -1,20 +1,15 @@
 #include <math.h>
 #include <stdio.h>
 
-int foo(int a, int d){
-    for (int z = 1; z <= a; z++) {
-      printf("%s", (int)(floor(d / powf(2, a - z))) % 2 == 0 ? "X" : "O");
-    }
-    printf("\n");
-    if (d-1 >= 0)
-    {
-        foo(a, d-1);
-    }
-}
-
 int main() {
   int a;
   scanf("%d", &a);
-  foo(a, powf(2, a)-1)
+  int c = powf(2, a);
+  for (int i = c - 1; i >= 0; i--) {
+    for (int z = 1; z <= a; z++) {
+      printf("%s", (int)(floor(i / powf(2, a - z))) % 2 == 0 ? "X" : "O");
+    }
+    printf("\n");
+  }
   return 0;
 }
