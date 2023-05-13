c7d25bd8f7977b2374b6a9af735fc8fe3c6554ad
wbhaao <a31620504@gmail.com>
2023-05-13 19:54:36 +0900

    진짜 모르곗다

--- baekjoon/차이를최대로10819.py
+++ baekjoon/차이를최대로10819.py
@@ -2,22 +2,57 @@ a = int(input())
 b = list(map(int, input().split()))
 c = 0
 f = 0
+y = 0
+s = 0
 
 b.sort()
 d = b[0:a//2]
 e = b[a//2:]
 e.sort(reverse=True)
 e[0], e[len(e)-1] = e[len(e)-1], e[0]
-for i in range(a//2):
-    c += abs(e[i] - d[i])
-    if i!=a//2-1:c += abs(d[i] - e[i+1])
+d.insert(1, e[len(e)-1])
+del e[len(e)-1]
+for i in range(a-1):
+    try:c += abs(e[i] - d[i])
+    except: pass
+    try:c += abs(d[i] - e[i+1])
+    except: pass
+    
     
 d = b[0:a//2]
 e = b[a//2:]
 e.sort(reverse=True)
 d[0], d[len(d)-1] = d[len(d)-1], d[0]
-for i in range(a//2):
-    f += abs(d[i] - e[i])
-    if i!=a//2-1:f += abs(e[i] - d[i+1])
+d.insert(1, d[len(d)-1])
+del d[len(d)-1]
+for i in range(a-1):
+    try:f += abs(d[i] - e[i])
+    except: pass
+    try:f += abs(e[i] - d[i+1])
+    except: pass
     
-print(max(c, f))
+d = b[0:a//2+1]
+e = b[a//2+1:]
+e.sort(reverse=True)
+d[0], d[len(d)-1] = d[len(d)-1], d[0]
+d.insert(1, d[len(d)-1])
+del d[len(d)-1]
+for i in range(a-1):
+    try:y += abs(d[i] - e[i])
+    except: pass
+    try:y += abs(e[i] - d[i+1])
+    except: pass
+        
+d = b[0:a//2+1]
+e = b[a//2+1:]
+e.sort(reverse=True)
+e[0], e[len(e)-1] = e[len(e)-1], e[0]
+e.insert(1, e[len(e)-1])
+del e[len(e)-1]
+for i in range(a-1):
+    try:s += abs(e[i] - d[i])
+    except: pass
+    try:s += abs(d[i] - e[i+1])
+    except: pass
+    
+print(max([c, f, y, s]))
