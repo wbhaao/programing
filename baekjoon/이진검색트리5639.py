7165a638c95359df7ba8ed92159843c8f1616a9d
wbhaao <a31620504@gmail.com>
2023-06-05 12:41:21 +0900

    이진검색트리 설명

--- baekjoon/이진검색트리5639.py
+++ baekjoon/이진검색트리5639.py
@@ -1,33 +1,34 @@
-lst = []
+import sys
+# 재귀 더 많이 쓸 수 있게 해줌
+sys.setrecursionlimit(10**6)
+num_list = []
+# 입력받기
 while True:
     try:
-        lst.append(int(input()))
+        num_list.append(int(input()))
     except:
         break
 
-root = int(lst[0])
-
-lst.sort()
-n = int(lst[0])
-i = 0
-iss = True
-while root>lst[i]:
-    print(lst[i])
-    if iss: 
-        i+=2 
-        if i!=2:
-            i += 1
-        iss = not iss
-    else: i-=1; iss = not iss
-
-lst.sort(reverse=True)
-i = 0
-iss = True
-while root<lst[i]:
-    print(lst[i])
-    if iss: 
-        i+=2 
-        if i!=2:
-            i += 1
-        iss = not iss
-    else: i-=1; iss = not iss
+def postorder(first,end):
+    # 
+    if first > end:
+        return
+    # 루트보다 큰 값이 존재하지 않을 경우를 대비
+    # 이렇게 된다면 위 if에 걸림
+    # 그게 아니고 큰게 없으면 안잘라도 됨 (root 빼고)
+    mid = end+1
+    # 1 ~ 3까지 (임의(0, 2일때)     
+    for i in range(first+1,end+1):
+        # 첫번째보다 큰게 있다면
+        if num_list[first] < num_list[i]:
+            mid = i
+            break
+    
+    #왼쪽 트리
+    postorder(first+1, mid-1)
+    #오른쪽 트리
+    postorder(mid, end)
+    # 중앙(은 가장 마지막)
+    print(num_list[first])
+# 0부터 numlist-1까지
+postorder(0,len(num_list)-1)
