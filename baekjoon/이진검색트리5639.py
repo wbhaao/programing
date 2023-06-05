ffb28d72ab36db99c09a9db68d290fbc2dbd7b9b
wbhaao <a31620504@gmail.com>
2023-06-05 13:50:22 +0900

    이진검색트리 이진으로 못풀겠다

--- baekjoon/이진검색트리5639.py
+++ baekjoon/이진검색트리5639.py
@@ -11,24 +11,22 @@ while True:
 
 def postorder(first,end):
     # 
-    if first > end:
+    if first>end:
         return
-    # 루트보다 큰 값이 존재하지 않을 경우를 대비
-    # 이렇게 된다면 위 if에 걸림
-    # 그게 아니고 큰게 없으면 안잘라도 됨 (root 빼고)
     mid = end+1
-    # 1 ~ 3까지 (임의(0, 2일때)     
-    for i in range(first+1,end+1):
-        # 첫번째보다 큰게 있다면
-        if num_list[first] < num_list[i]:
-            mid = i
-            break
-    
-    #왼쪽 트리
+    f, e = int(first+1), int(end+1)
+    while f<=e:
+        middle = (f+e)//2
+        if num_list[first] < num_list[middle]:
+            e = middle-1
+            mid = middle
+        else:
+            f = middle+1
+           
+    # 전위
     postorder(first+1, mid-1)
-    #오른쪽 트리
+    # 후위
     postorder(mid, end)
-    # 중앙(은 가장 마지막)
     print(num_list[first])
 # 0부터 numlist-1까지
 postorder(0,len(num_list)-1)
