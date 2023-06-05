2cc537f8f747b464d3a558e874df8c8c19441831
wbhaao <a31620504@gmail.com>
2023-06-05 22:16:16 +0900

    Kruskal 알고리즘 사용한 최소스패닝트리

--- baekjoon/최소스패닝트리.py
+++ baekjoon/최소스패닝트리.py
@@ -1,43 +1,36 @@
 import sys
-sys.setrecursionlimit(10**6)
-V, E = map(int, sys.stdin.readline().split())
-parent = [x for x in range(V+1)]
-edges = []
-
+input = sys.stdin.readline
+ 
+V, E = map(int, input().split())
+Vroot = [i for i in range(V+1)]
+Elist = []
 for _ in range(E):
-    A, B, C = map(int, sys.stdin.readline().split())
-    edges.append((A, B, C))
+    Elist.append(list(map(int, input().split())))
 
-
-def find_parent(x):
-    if parent[x] == x:
-        return x
-    else:
-        parent[x] = find_parent(parent[x])
-        return parent[x]
-
-
-def union(a, b):
-    pa = find_parent(a)
-    pb = find_parent(b)
-
-    if pa < pb:
-        parent[pb] = pa
-    else:
-        parent[pa] = pb
-
-
+# x[2] 기준으로 정렬
+Elist.sort(key=lambda x: x[2])
+ 
+ 
+def find(x):
+    if x != Vroot[x]:
+        Vroot[x] = find(Vroot[x])
+    return Vroot[x]
+ 
+ 
 answer = 0
-edges.sort(key=lambda x: x[2])
-
-for edge in edges:
-    x, y, cost = edge[0], edge[1], edge[2]
-    # 사이클을 만들면 무시
-    if find_parent(x) == find_parent(y):
-        continue
-
-    union(x, y)
-    answer += cost
-
-
+# s: 노드1, e: 노드2, w: 가중치
+for s, e, w in Elist:
+    sRoot = find(s)
+    eRoot = find(e)
+    if sRoot != eRoot:
+        # 간선 연결
+        if sRoot < eRoot:
+            # sRoot는 eRoot와 연결되어있다
+            Vroot[sRoot] = eRoot
+        else:
+            # eRoot는 sRoot와 연결되어있다
+            Vroot[eRoot] = sRoot
+        # 가중치 더하기
+        answer += w
+ 
 print(answer)
