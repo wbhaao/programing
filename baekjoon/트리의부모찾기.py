d73049b94aef07c548956cc36f884cea1d4c644c
wbhaao <a31620504@gmail.com>
2023-06-09 17:16:15 +0900

    안보고 풀었다리

--- baekjoon/트리의부모찾기.py
+++ baekjoon/트리의부모찾기.py
@@ -3,10 +3,7 @@ sys.setrecursionlimit(10**6)
 
 V = int(input())
 graph = [[] for _ in range(V+1)]
-visited = [0]*(V+1)
 result = [0]*(V+1)
-visited[0] = 1
-cur = 2
 # 그래프와의 관계를 나타냄
 for _ in range(V-1):
     a, b = map(int, input().split())
@@ -14,20 +11,10 @@ for _ in range(V-1):
     graph[b].append(a)
 
 def find_Path(curNode, parNode):
-    # curNode : 현재 노드
-    # i : 연결노드
-    
-    for i in range(1, V+1):
-        global cur
-        if i in graph[curNode] and visited[i]==0 and i!=parNode:
-            if i == cur:
-                cur+=1
-                # visited[i] = 1
-                result[i] = curNode
-                find_Path(i, curNode)
-            else:
-                find_Path(i, curNode)
-while cur<=V:
-    find_Path(1, 0)
-for i in range(1, V+1):
+    for g in graph[curNode]:
+        if g!=parNode:
+            result[g] = curNode
+            find_Path(g, curNode)
+find_Path(1, 0)
+for i in range(2, V+1):
     print(result[i])
