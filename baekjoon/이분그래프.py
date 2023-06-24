1a1bf7cbae465c65e7a43c93fc64bff32e1e1c82
wbhaao <a31620504@gmail.com>
2023-06-24 11:36:18 +0900

    이분그래프 정답

--- baekjoon/이분그래프.py
+++ baekjoon/이분그래프.py
@@ -1,35 +1,44 @@
-from collections import deque
 import sys
-read = sys.stdin.readline
-color = 0
-cnt = read()
-for i in range(cnt):
+sys.setrecursionlimit(10**6)
+k = int(input())
+
+
+def DFS(start, visited, graph, group):
+    visited[start] = group  # 현재 노드의 그룹 저장
+
+    # 인접 노드 탐색
+    for v in graph[start]:
+        if visited[v] == 0:  # 아직 방문하지 않은 노드
+            # -group : 현재 노드의 그룹과 다른 값 전달
+            # visited, graph는 리스트 넘겨주는 거라 상관 x
+            result = DFS(v, visited, graph, -group)
+            if not result:
+                return False
+        else:
+            if visited[v] == group:  # 이미 방문한 곳 중에서 노드가 현재 그룹과 같으면 이분 그래프가 아님
+                return False
+    return True
+
+# k 번 반복
+for _ in range(k):
     # 정점 개수, 간선 개수
-    n, m = map(int, read().split())
-    graph = [[0] * (n + 1) for _ in range(n + 1)] 
-    # [visited, color]
-    # red : 0, blue : 1
-    visit_list2 = [0,0] * (n + 1)
-    for z in range(m):
-        a, b = map(int, read().split())
-        graph[a][b] = 1
-        graph[b][a] = 1
-        
-    q = deque(1)
-    visit_list2[0] = [1, color]
-    
-    # queue를 통해 우선순위 정하면서
-    while q:
-        # 현재 노드를 대기열에서 삭제
-        vv = q.popleft()
-        # 현재 노드 출력
-        print(vv, end=" ")
-        # 주변 노드 있으면 대기열에 저장 (작은순)
-        for z in range(1, n+1):
-            if color == visit_list2[z][1]:
+    V, E = map(int, sys.stdin.readline().split())
+    graph = [[] for _ in range(V+1)]
+    visited = [0] * (V+1)
+    for _ in range(E):
+        # 각 정점 추가
+        a, b = map(int, sys.stdin.readline().split())
+        # 정점 연결
+        graph[a].append(b)
+        graph[b].append(a)
+    # 정점끼리 탐색
+    for i in range(1, V+1):
+        # 방문하지 않았다면 (DFS도중 방문할 수 있음)
+        if visited[i] == 0:
+            result = (DFS(i, visited, graph, 1))
+            if not result:
                 break
-            if graph[vv][z] == 1 and visit_list2[z][0]==0:
-                q.append(z)
-                visit_list2[z] = 1
-    # color 반전
-    color ^= 1
+    if result: 
+        print("YES") 
+    else: 
+        print("NO")
