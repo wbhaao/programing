cc91dd822b39160eb3ca2a7097a1823d22f75f03
wbhaao <a31620504@gmail.com>
2023-06-27 17:06:05 +0900

    팰린드롬 수 정답

--- baekjoon/팰린드롬수.py
+++ baekjoon/팰린드롬수.py
@@ -1,10 +1,8 @@
-import sys
-import math
-read = sys.stdin.readline
 while True:
-  num = str(int(read()))
-  if (num=="0"): break
-  if (str(num[:(len(num)//2)]) == str(list(num[math.ceil(len(num)/2):])[::-1])):
-    print("yes")
-  else:
-    print("no")
+    n = input()
+    if n == '0':
+        break
+    if n== n[::-1]:
+        print('yes')
+    else:
+        print('no')
