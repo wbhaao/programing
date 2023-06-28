765c22e23432dc569613a94d8064ba991cfeda0e
wbhaao <a31620504@gmail.com>
2023-06-28 20:29:09 +0900

    설명도 추가

--- baekjoon/특정거리의도시찾기.py
+++ baekjoon/특정거리의도시찾기.py
@@ -1,43 +1,40 @@
-'''
-이제 BFS가 진짜 쉬워졌다.
-'''
 from collections import deque
 import sys
+
 read = sys.stdin.readline
-'''
-N : 도시의 개수
-M : 도로의 개수
-K : 거리 정보
-X : 출발 도시의 번호
-'''
 N, M, K, X = map(int, read().split())
-visited = [0]*N
-distance = [0]*N
-graph = [[] for _ in range(N)]
-for _ in range(M):
+visited = [0]*(N+1)
+graph = [[] for _ in range(N+1)]
+for i in range(M):
   # 오퍼레이터, 오퍼랜드 : 그냥 연산자, 피연산자 뜻, 
   oprt, oprd = map(int, read().split())
-  graph[oprt-1].append(oprd-1)
+  graph[oprt].append(oprd)
 
 def BFS():
   result = []
   q = deque()
-  q.append(X-1)
-  visited[X-1] = 1
-  distance[X-1] = 0
+  q.append((X, 1))
+  visited[X] = 1
   while q:
-    curNode = q.popleft()
+    curNode, time = q.popleft()
     for i in graph[curNode]:
       if visited[i] == 0:
-          visited[i] = 1
-          q.append(i)
-          distance[i] = distance[curNode] + 1
-          if distance==K:
-            result.append(i)
-  if not result:
-    print(-1)
-  else:
-    result.sort()
-    for j in result:
-      print(j, end='\n')
-BFS()
+        q.append((i, time+1))
+        # 정답이면 visited를 1로 안했었는데
+        # 정답이라도 들어갔다 해야한다
+        # https://uncovered-grain-c53.notion.site/cecfd5b39701414f899de5abb6148441?pvs=4
+        # 이해돕기
+        visited[i] = 1
+        if time==K:
+          result.append(i)
+        elif time>K:
+          break
+        
+  return result
+bfs = BFS()
+if not bfs:
+  print(-1)
+else:
+  bfs.sort()
+  for i in bfs:
+    print(i)
