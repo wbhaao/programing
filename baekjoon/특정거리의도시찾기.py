60eec6a475e71bb0597242c0b78c315f290d48d9
wbhaao <a31620504@gmail.com>
2023-06-28 19:59:23 +0900

    복붙이지만 동작이 안됨

--- baekjoon/특정거리의도시찾기.py
+++ baekjoon/특정거리의도시찾기.py
@@ -3,7 +3,6 @@
 '''
 from collections import deque
 import sys
-sys.setrecursionlimit(10**6)
 read = sys.stdin.readline
 '''
 N : 도시의 개수
@@ -11,33 +10,34 @@ M : 도로의 개수
 K : 거리 정보
 X : 출발 도시의 번호
 '''
-N, M, K, X = map(int, input().split())
+N, M, K, X = map(int, read().split())
 visited = [0]*N
+distance = [0]*N
 graph = [[] for _ in range(N)]
-for i in range(M):
+for _ in range(M):
   # 오퍼레이터, 오퍼랜드 : 그냥 연산자, 피연산자 뜻, 
-  oprt, oprd = map(int, input().split())
+  oprt, oprd = map(int, read().split())
   graph[oprt-1].append(oprd-1)
 
 def BFS():
-  q = deque()
-  q.append((X-1, 1))
   result = []
+  q = deque()
+  q.append(X-1)
+  visited[X-1] = 1
+  distance[X-1] = 0
   while q:
-    curNode, time = q.popleft()
+    curNode = q.popleft()
     for i in graph[curNode]:
       if visited[i] == 0:
-        if time==K:
-          result.append(i+1)
-        elif time>K:
-          break
-        else:
           visited[i] = 1
-          q.append((i, time+1))
-  return result
-bfs = BFS()
-if not bfs:
-  print(-1)
-else:
-  for i in bfs:
-    print(i)
+          q.append(i)
+          distance[i] = distance[curNode] + 1
+          if distance==K:
+            result.append(i)
+  if not result:
+    print(-1)
+  else:
+    result.sort()
+    for j in result:
+      print(j, end='\n')
+BFS()
