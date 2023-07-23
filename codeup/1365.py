e4a69258e90c3ff7b9f4bc99b3bad1ad8a6731d8
wbhaao <a31620504@gmail.com>
2023-07-23 18:49:50 +0900

    1365

---
+++ codeup/1365.py
@@ -0,0 +1,8 @@
+n = int(input())
+for i in range(n):
+    for z in range(n):
+        if i==0 or i==n-1 or z==0 or z==n-1 or i==z or i+z==n-1:
+            print('*', end="")
+        else:
+            print(' ', end="")
+    print()
