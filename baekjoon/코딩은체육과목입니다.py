26c0192a53032ad3a9041ae3c7a137740e355cc6
wbhaao <a31620504@gmail.com>
2023-07-23 18:31:23 +0900

    코딩은체육과목입니다

---
+++ baekjoon/코딩은체육과목입니다.py
@@ -0,0 +1,5 @@
+N = int(input())
+result = ""
+for i in range(N//4):
+    result += 'long '
+print(result+'int')
--- codeup/1451.c
+++ codeup/1451.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 int main(){
     int a=0, b=0;
-    int lst[10001];
+    int lst[100000001];
     scanf("%d", &a);
     for ( int i = 0; i < a; i++ ){
         scanf("%d", &lst[i]);
     }
     for ( int j = 0; j < a; j++ ){
         for ( int x = j; x < a; x++ ){
-            if (lst[j] < lst[x]){
+            if (lst[j] > lst[x]){
                 int temp = lst[j];
                 lst[j] = lst[x];
                 lst[x] = temp;
