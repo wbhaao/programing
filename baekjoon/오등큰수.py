b6017cdd5869fb99cfbd763256f0d8b42ffabfbc
wbhaao <a31620504@gmail.com>
2023-07-28 04:32:20 +0900

    오등큰수

--- baekjoon/오등큰수.py
+++ baekjoon/오등큰수.py
@@ -1,13 +1,18 @@
 import sys
+from collections import Counter
+
 n = int(input())
 A = list(map(int, sys.stdin.readline().split()))
 answer = [-1] * n
+count = Counter(A) 
 stack = []
 # 비교당할 애
 stack.append(0)
-# 1부터 n-1까지
+# 1부터 n-1까지ㄴ
+# A[i]는 1, 2, 3
+# count[A[i]]는 그 숫자가 등장한 번수
 for i in range(1, n):
-    while stack and A[i] > A[stack[-1]]:
+    while stack and count[A[i]] > count[A[stack[-1]]]:
         answer[stack.pop()] = A[i]
     stack.append(i)
 print(*answer)
