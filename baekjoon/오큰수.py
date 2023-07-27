6f7da23c8ae77a939874797ad01484161f77d00e
wbhaao <a31620504@gmail.com>
2023-07-28 03:36:20 +0900

    오큰수

--- baekjoon/오큰수.py
+++ baekjoon/오큰수.py
@@ -1,14 +1,17 @@
-a = int(input())
-lst = list(map(int, input().split()))
+import sys
+n = int(input())
+A = list(map(int, sys.stdin.readline().split()))
+answer = [-1] * n
 stack = []
-result = -1
-for i in range(len(lst)-1):
-    for j in range(i+1, len(lst)):
-        if  lst[i] < lst[j]:
-            result = lst[j]
-            print(result, end=" ")
-            result = -1
-            break
-    if result == -1:
-        print(result, end=" ")
-print(-1, end=" ")
+# 비교당할 애
+stack.append(0)
+# 1부터 n-1까지
+for i in range(1, n):
+    # stack이 남아있고, 맨 뒤 스택보다 비교할 애가 더 크면
+    while stack and A[stack[-1]] < A[i]:
+        # 오큰수 찾은거 
+        answer[stack.pop()] = A[i]
+    # 비교한애 오큰수도 찾게 append
+    stack.append(i)
+
+print(*answer)
