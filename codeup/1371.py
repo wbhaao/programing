5dc4bcfc4f3e404839401cb4d721796644017cc9
wbhaao <a31620504@gmail.com>
2023-08-01 21:31:41 +0900

    3122

--- codeup/1371.py
+++ codeup/1371.py
@@ -2,11 +2,13 @@ N = int(input())
 
 for i in range(N):
     print(" " * (N-i-1), end="")
-    print("*" * (i*2), end="")
+    print("*", end="")
+    print(" " * (i*2), end="")
     print("*", end="")
     print()
-for i in reversed(range(N-1)):
+for i in reversed(range(N)):
     print(" " * (N-i-1), end="")
-    print("*" * (i*2), end="")
+    print("*", end="")
+    print(" " * (i*2), end="")
     print("*", end="")
     if i!=0: print()
