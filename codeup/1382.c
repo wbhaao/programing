7edc4d2a84b8226ebb8fa2aee1575c94d4f3d41e
wbhaao <a31620504@gmail.com>
2023-08-01 20:19:48 +0900

    1382

---
+++ codeup/1382.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+ 
+int main(){
+    for(int i = 1; i <= 9;i++){
+        for(int x = 2; x <= 5; x++){
+            printf("%d x %d = %2d\t", x, i, x*i);
+        }
+        printf("\n");
+    }
+    
+    return 0;
+}
