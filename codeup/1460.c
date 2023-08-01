613daebdb907ebdef9c961930615e81e61627ccd
wbhaao <a31620504@gmail.com>
2023-08-01 21:52:35 +0900

    1460

---
+++ codeup/1460.c
@@ -0,0 +1,23 @@
+#include "stdio.h"
+ 
+int main()
+{
+    int arr[101][101];
+    
+    int num;
+    scanf("%d", &num);
+    int x = 1;
+    for(int i = 1; i <=num; i++){
+        for(int j = 1; j<= num; j++){
+            arr[i][j] = x;
+            x++;
+        }
+    }
+    for(int i = 1; i <= num; i++){
+        for(int j = 1; j <= num; j++){
+            printf("%d ",arr[i][j]);
+        }
+        printf("\n");
+    }
+    return 0;
+}
