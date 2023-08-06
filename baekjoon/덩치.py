4cf5444e3f5242006d44f7f92d609298ceb52b76
wbhaao <a31620504@gmail.com>
2023-08-07 07:09:16 +0900

    덩치 더 빠름

--- baekjoon/덩치.py
+++ baekjoon/덩치.py
@@ -1,11 +1,13 @@
 N = int(input())
 lst = []
-
+gList = [1] * N
 for i in range(N):
     lst.append(list(map(int, input().split())))
 for i in range(N):
     grade = 1
-    for j in range(N):
+    for j in range(i, N):
         if lst[i][0] < lst[j][0] and lst[i][1] < lst[j][1]:
-            grade += 1
-    print(grade, end=" ")
+            gList[i] += 1
+        if lst[i][0] > lst[j][0] and lst[i][1] > lst[j][1]:
+            gList[j] += 1
+print(*gList)
