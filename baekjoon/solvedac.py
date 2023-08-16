71a90b5978cb187efe37df2de0ceb74a1a9a54fb
wbhaao <a31620504@gmail.com>
2023-08-16 10:09:12 +0900

    커밋채우기

--- baekjoon/solvedac.py
+++ baekjoon/solvedac.py
@@ -1,15 +1,25 @@
-N = int(input())
-lst2 = [0] * N
-lst = [0] * N
-cnt = 0
-for i in range(N):
-    lst2[i] = int(input())
-lst2.sort()
-for i in range(N):
-    if i >= round(N * 0.15) and i <= round(N * 0.85):
-        lst[i] = lst2.pop(round(N * 0.15))
-        cnt += 1
-try:
-    print(round(sum(lst) / cnt))
-except:
-    print(0)
+import sys
+
+
+def my_round(num):
+    if num - int(num) >= 0.5:
+        return int(num) + 1
+    else:
+        return int(num)
+    # == return int(num) + (1 if num - int(num) >= 0.5 else 0)
+
+
+if __name__ == "__main__":
+    n = int(sys.stdin.readline())
+    if not n:
+        print(0)
+    else:
+        difficulty = [int(sys.stdin.readline()) for _ in range(n)]
+        difficulty.sort()
+        exception = my_round(n * 0.15)
+
+        if exception != 0:
+            difficulty = difficulty[exception:-exception]
+        cnt = n - 2 * exception
+        result = sum(difficulty)
+        print(my_round(result / cnt))
