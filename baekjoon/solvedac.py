e5f51040e9f23393c4dac2653c932605bd0acf8e
wbhaao <a31620504@gmail.com>
2023-08-17 09:57:50 +0900

    6069

--- baekjoon/solvedac.py
+++ baekjoon/solvedac.py
@@ -1,25 +1,25 @@
 import sys
 
+input = sys.stdin.readline
+
 
 def my_round(num):
     if num - int(num) >= 0.5:
         return int(num) + 1
     else:
         return int(num)
-    # == return int(num) + (1 if num - int(num) >= 0.5 else 0)
 
 
-if __name__ == "__main__":
-    n = int(sys.stdin.readline())
-    if not n:
-        print(0)
-    else:
-        difficulty = [int(sys.stdin.readline()) for _ in range(n)]
-        difficulty.sort()
-        exception = my_round(n * 0.15)
+n = int(input())
+if not n:
+    print(0)
+else:
+    diff = [int(input()) for _ in range(n)]
+    diff.sort()
+    expt = my_round(n * 0.15)
 
-        if exception != 0:
-            difficulty = difficulty[exception:-exception]
-        cnt = n - 2 * exception
-        result = sum(difficulty)
-        print(my_round(result / cnt))
+    if expt != 0:
+        diff = diff[expt:-expt]
+    cnt = n - 2 * expt
+    result = sum(diff)
+    print(my_round(result / cnt))
