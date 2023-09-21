aeead34e6f9846314a3d63f8b51652eb284ba365
wbhaao <a31620504@gmail.com>
2023-09-21 09:27:55 +0900

    자릿수계산

--- 2확기프그파이썬/자릿수계산.py
+++ 2확기프그파이썬/자릿수계산.py
@@ -1,1 +1,14 @@
-print(len(str(input())))
+n = int(input())
+def get_max_num(n):
+    num = 0
+    for i in range(1, n+1):
+        num += i
+    return num
+
+number = get_max_num(n)
+
+for i in range(1, n+1):
+    for j in range(0, i):
+        print(number, end=' ')
+        number -= 1
+    print()
