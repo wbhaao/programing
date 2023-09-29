c3880680b3f7f171528798f021b612fa2312dd38
김규민 <82032508+wbhaao@users.noreply.github.com>
2023-09-29 12:30:03 +0900

    4

--- pyauto.py
+++ pyauto.py
@@ -1,16 +1,16 @@
 def solution(N, W):
-    lst = [0] * 26
-    answer = []
-    sss = ""
-    for w in W:
-        lst[ord(w[0])-97] += 1
-        lst[ord(w[-1])-97] -= 1
-    cnt = 0
-    for i, l in enumerate(lst):
-        if l == 1:
-            sss = chr(i+97)
-            break
-    for w in W:
-        if w[0] == sss:
-            answer.append(w)
-    return answer
+    for i in range(N):
+        lst = [0] * 26
+        for j, w in enumerate(W):
+            if i != j:
+                lst[ord(w[0])-97] += 1
+                lst[ord(w[-1])-97] -= 1
+        cnt = 0
+        for l in lst:
+            if l != 0:
+                cnt += abs(l)
+        
+        if cnt <= 3:
+            return "Yes"
+    return "No"
+print(solution(6, ["apple","egg","graph","hi","busy","yield"]))
