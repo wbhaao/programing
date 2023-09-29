75403c21bbdb8492f15106cfb074f8a81e18d76e
김규민 <82032508+wbhaao@users.noreply.github.com>
2023-09-29 12:49:51 +0900

    5 중간점검

--- pyauto.py
+++ pyauto.py
@@ -1,16 +1,16 @@
 def solution(N, W):
+    answer = []
     for i in range(N):
-        lst = [0] * 26
-        for j, w in enumerate(W):
-            if i != j:
-                lst[ord(w[0])-97] += 1
-                lst[ord(w[-1])-97] -= 1
-        cnt = 0
-        for l in lst:
-            if l != 0:
-                cnt += abs(l)
-        
-        if cnt <= 3:
-            return "Yes"
-    return "No"
-print(solution(6, ["apple","egg","graph","hi","busy","yield"]))
+        WW = list(W)
+        w = WW.pop(i)
+        while True:
+            for w2 in WW:
+                if w[-1] in w2:
+                    w = str(w2)
+                    WW.remove(w2)
+                    break
+            if w != w2:
+            
+
+    return answer
+print(solution(5, ["abc","chocolate","everyday","hi","essential"]))
