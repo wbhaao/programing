90e6484d9de6d0d1978647a061e8bc0121f3e301
김규민 <82032508+wbhaao@users.noreply.github.com>
2023-10-04 12:06:36 +0900

    계단오르기

---
+++ 2확기프그파이썬/계단.py
@@ -0,0 +1,29 @@
+N = int(input())
+lst = list(map(int, input().split()))
+dp = [0] * N
+
+def w(i, N):
+    if i==N:
+        return
+    dp[i] = max(dp[i-3]+lst[i-1]+lst[i], dp[i-2]+lst[i])
+    w(i+1, N)
+
+if len(lst) <= 2:
+    print(sum(lst))
+else:
+    dp[0] = lst[0]
+    dp[1] = lst[0] + lst[1]
+    w(2, N)
+    print(dp[-1])
+
+
+
+
+
+
+
+
+
+
+
+
