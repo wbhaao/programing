eaf1c6ca047518f092fecab0334ebe1e2946bce1
김규민 <82032508+wbhaao@users.noreply.github.com>
2023-11-19 17:08:24 +0900

    string 2

--- 자바/String1.java
+++ 자바/String1.java
@@ -5,5 +5,10 @@ public class String1 {
         String str2 = "안녕";
         String str3 = "안녕";
         String str4 = new String("안녕");
+         // 스택 메모리값 비교(==)
+        System.out.println(str1 == str2);
+        System.out.println(str2 == str3);
+        System.out.println(str3 == str4);
+        System.out.println(str4 == str1);
     }
 }
