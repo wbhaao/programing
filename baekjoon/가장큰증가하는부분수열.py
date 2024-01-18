61a431b16af49c0cbebe17a6bfc49cd5ddc919bf
wbhaao <a31620504@gmail.com>
2024-01-18 12:24:26 +0900

    가장큰 증가하는 부분수열

--- baekjoon/가장큰증가하는부분수열.py
+++ baekjoon/가장큰증가하는부분수열.py
@@ -1,7 +1,21 @@
+# LIS 알고리즘
+'''
+알고리즘
+순차적으로 순회하면서
+dp[i] 값을 계속 + 함
+알고리즘이 잘못되었담ㄴ어ㅐ뮤ㅒ냐ㅗ해ㅑㅑㅗ랴ㅒ
+'''
+
 N = int(input())
-lst = list(map(int, input().split()))
-dp = [0] * N
-dp[0] = lst[0]
+
+numbers = list(map(int, input().split()))
+dp = list(numbers)
+dp[0] = numbers[0]
 for i in range(1, N):
-    if lst[i]:
-        dp[i] = 0
+    prev = numbers[i]
+    for j in reversed(range(i)):
+        # prev 선언
+        if prev > numbers[j]:
+            dp[i] += numbers[j]
+            prev = numbers[j]
+print(max(dp))
