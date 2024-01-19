843e8c2b939360641e863fd4cc27ce91caac8907
wbhaao <a31620504@gmail.com>
2024-01-19 13:58:35 +0900

    가장큰증가하는부분수열

--- baekjoon/가장큰증가하는부분수열.py
+++ baekjoon/가장큰증가하는부분수열.py
@@ -7,17 +7,22 @@ dp[i] 값을 계속 + 함
 
 
 '''
-
 N = int(input())
-
 numbers = list(map(int, input().split()))
-dp = list(numbers)
+
+# 왜 1이지, 0이여도 상관 X
+dp = [0]*N
+# 0번지까지 numbers의 최댓값은 numbers[0]
 dp[0] = numbers[0]
+# 1 index부터 채움
 for i in range(1, N):
-    prev = numbers[i]
-    for j in reversed(range(i)):
-        # prev 선언
-        if prev > numbers[j]:
-            dp[i] += numbers[j]
-            prev = numbers[j]
+    # 0부터 순차적으로 검사
+    for j in range(i):
+        # 기준값이 더할 값보다 크다면
+        if numbers[j] < numbers[i]:
+            # dp[i] = 원래값(for문 n번째 전)과 dp[j]번과 numbers 더한 값 중 큰 값 고름
+            dp[i] = max(dp[i], dp[j] + numbers[i])
+        else:
+            # numbers[i]가 더 크니 이전 값을 쓸 수 없음. numbers[i]와만 비교
+            dp[i] = max(dp[i], numbers[i])
 print(max(dp))
