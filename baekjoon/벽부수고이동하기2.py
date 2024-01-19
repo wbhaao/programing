c80eababe823917c17c37620526bfb21ddb56849
wbhaao <a31620504@gmail.com>
2024-01-19 16:59:28 +0900

    벽부수고이동하기 모법답안

--- baekjoon/벽부수고이동하기2.py
+++ baekjoon/벽부수고이동하기2.py
@@ -1,26 +1,42 @@
+'''
+근데 3차원말고 q에 w 집어넣으면 안되나??????
+해보자!
+라고 하기엔 너무 피곤하다........
+음..
+주석달아야지
+'''
+
 from collections import deque
-from copy import deepcopy
+
 N, M, K = map(int, input().split())
 lst = [list(input()) for _ in range(N)]
 
-q = deque()
-visited = [[0]*M for _ in range(N)]
-visited[0][0] = 1
-q.append((0, 0, 0, 1, deepcopy(visited)))
-while q:
-    y, x, k, depth, visit = q.popleft()
-    for iy, ix in [(y, x+1), (y+1, x), (y, x-1), (y-1, x)]:
-        if 0 <= iy < N and 0 <= ix < M and visit[iy][ix] == 0:
-            visit[iy][ix] = 1
-            if iy==N-1 and ix==M-1:
-                print(depth+1)
-                exit()
-            if lst[iy][ix] == '1':
-                if k+1 > K:
-                    continue
-                else:
-                    q.append((iy, ix, k+1, depth+1, deepcopy(visit)))
-            else:
-                q.append((iy, ix, k, depth+1, deepcopy(visit)))
-            visit[iy][ix] = 0
+dx = [0, 0, -1, 1]
+dy = [1, -1, 0, 0]
+
+visited = [[[0]*(K+1) for _ in range(M)] for __ in range(N)]
+queue = deque([(0, 0, 0)])
+
+# 초기 설정
+visited[0][0][0] = 1
+
+while queue:
+    x, y, w = queue.popleft()
+
+    # 끝에 도달한 경우
+    if x == N-1 and y == M-1:
+        print(visited[x][y][w])
+        exit(0)
+
+    for i in range(4):
+        nx, ny = x + dx[i], y + dy[i]
+        if nx < 0 or nx >= N or ny < 0 or ny >= M:
+            continue
+        if lst[nx][ny] == '1' and w < K and visited[nx][ny][w+1] == 0:
+            visited[nx][ny][w+1] = visited[x][y][w] + 1
+            queue.append((nx, ny, w+1))
+        elif lst[nx][ny] == '0' and visited[nx][ny][w] == 0:
+            visited[nx][ny][w] = visited[x][y][w] + 1
+            queue.append((nx, ny, w))
+
 print(-1)
