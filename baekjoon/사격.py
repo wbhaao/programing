54d75499fa29777b246c69bd0832e34907eb3a3c
wbhaao <a31620504@gmail.com>
2024-01-22 09:11:01 +0900

    gpt한테 물어봤지만 당연히 안됌

--- baekjoon/사격.py
+++ baekjoon/사격.py
@@ -1,21 +1,20 @@
-target_cnt, bullet_cnt, cutoff_score = map(int, input().split())
-target_list = list(map(int, input().split()))
-# 걍 제일 높은거 하나 맞추면 되잖아 ???
-# 문제가 왜 이따구
-# 가 아니였고 실력은 항상 늠
-# 완전탐색!
-level = 1
-score = 0
-target_list.sort(reverse=True)
-for i in range(1, 100000001):
-    level = i
-    for _ in range(bullet_cnt):
-        for t in target_list:
-            if level >= t:
-                level += t
-                score += t
-                break
-    if score >= cutoff_score:
-        print(i)
-        exit()
-    score = 0
+import sys
+from bisect import bisect_right
+
+n, m, a = map(int, sys.stdin.readline().split())
+s = list(map(int, sys.stdin.readline().split()))
+s.sort()
+left, right = 1, max(s)
+
+while left < right:
+    mid = (left + right) // 2
+    idx = bisect_right(s, mid)
+    if idx == n:
+        total = sum(s) + (m-n)*s[-1]
+    else:
+        total = sum(s[idx:]) + (m-n+idx)*s[idx-1]
+    if total >= a:
+        right = mid
+    else:
+        left = mid + 1
+print(left)
