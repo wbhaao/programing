d0f9d88682682d1f31af87ba4e992eed3a508b3b
wbhaao <a31620504@gmail.com>
2024-02-07 22:15:16 +0900

    구간합구하기dp써서풀었다

--- baekjoon/구간합구하기.py
+++ baekjoon/구간합구하기.py
@@ -2,9 +2,9 @@ from sys import stdin
 input = stdin.readline
 N, M = map(int, input().split())
 lst = list(map(int, input().split()))
+dp = [0]*(N+1)
+for i in range(N):
+    dp[i + 1] = dp[i] + lst[i]
 for i in range(M):
     i, j = map(int, input().split())
-    if i==j:
-        print(lst[j-1])
-        continue
-    print(sum(lst[i-1:j]))
+    print(dp[j] - dp[i - 1])
