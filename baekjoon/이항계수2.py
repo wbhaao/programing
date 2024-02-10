bd332c1396b6ee91218a2b02f45030ed655f4b9c
wbhaao <a31620504@gmail.com>
2024-02-10 18:39:16 +0900

    뭔다이나믹프로그래밍이야이항계수2

---
+++ baekjoon/이항계수2.py
@@ -0,0 +1,16 @@
+from sys import setrecursionlimit
+setrecursionlimit(10**6)
+N, K = map(int, input().split())
+num = 1
+for i in range(K):
+    num *= N-i
+    
+def fac(K):
+    if K < 2:
+        return 1
+    else:
+        return K * fac(K-1)
+    
+kk = fac(K)
+result = num // kk
+print(result%10007)
