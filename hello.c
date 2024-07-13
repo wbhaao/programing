3c359f3ba612a1b365361e75b46207b383daebac
김규민 <82032508+wbhaao@users.noreply.github.com>
2024-07-13 15:32:53 +0900

    임베디드 스터디

---
+++ hello.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+void add(int *a, int b) {
+    *a += b;
+}
+
+int main() {
+    int a, b;
+    scanf("%d %d", &a, &b);
+    while (a < 100)
+    {
+        add(&a, b);
+        printf("%d\n", a);
+    }
+    return 0;
+}
